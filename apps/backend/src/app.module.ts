080e51b8b246044f15889d920982339ca5bff99b
Dmitrii Ivanov <codemecodeyou@gmail.com>
2026-05-02 22:46:32 +0300

    feat: update app module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -6,6 +6,11 @@ import { AuthModule } from './auth/auth.module';
 import { User } from './users/user.entity';
 import { RefreshToken } from './auth/refresh-token.entity';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { ChatsModule } from './chats/chats.module';
+import { Chat } from './chats/chat.entity';
+import { ChatParticipant } from './chats/chat_participant.entity';
+import { Message } from './messages/message.entity';
+import { Friendship } from './friendships/friendship.entity';
 
 @Module({
   imports: [
@@ -20,12 +25,20 @@ import { LoggerMiddleware } from './common/middleware/logger.middleware';
         username: config.getOrThrow<string>('DATABASE_USER'),
         password: config.getOrThrow<string>('DATABASE_PASSWORD'),
         database: config.getOrThrow<string>('DATABASE_NAME'),
-        entities: [User, RefreshToken],
+        entities: [
+          User,
+          RefreshToken,
+          Chat,
+          ChatParticipant,
+          Message,
+          Friendship,
+        ],
         synchronize: config.get<string>('NODE_ENV') === 'development',
       }),
     }),
 
     AuthModule,
+    ChatsModule,
   ],
 })
 export class AppModule implements NestModule {

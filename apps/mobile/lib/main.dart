5a7380159bd6ee83b02fe1d8f01b25e516a65245
Dmitrii Ivanov <codemecodeyou@gmail.com>
2026-09-02 23:33:56 +0300

    update locks

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
+import 'package:flutter_driver/driver_extension.dart';
 
 void main() {
+  if (const bool.fromEnvironment('ENABLE_FLUTTER_DRIVER')) {
+    enableFlutterDriverExtension();
+  }
+
   runApp(const MyApp());
 }
 
